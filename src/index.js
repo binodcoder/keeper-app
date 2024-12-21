2bff6e22adeb390ac849944405f3a52bc7390803
binodcoder <binodcoder@gmail.com>
2024-12-21 21:34:05 +0000

    first stage of an app;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
 //1. Create a new React app.
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
@@ -10,3 +14,5 @@
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+ReactDOM.render(<App />, document.getElementById("root"));
